7e5f0b8c28b94e9894c70196fe185b5258786984
Cleo Schneider <cleo.m.schneider@gmail.com>
2016-06-04 13:35:17 -0400

    adding oath for google calendar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, response
 
 
 app = Flask('mind-me', static_url_path='')
@@ -7,6 +7,18 @@ app = Flask('mind-me', static_url_path='')
 @app.route('/')
 def index():
     return jsonify(status='ok', data='hello')
+
+@app.route('/oauth2_callback')
+def oauth_redirect():
+	content = response.content
+	import pdb; pdb.set_trace()
+	return jsonify(status='ok', response=response.content)
+
+@app.route('/calendar')
+def authenticate():
+	from calendar.oauth2 import connect_calendar
+	connect_calendar()
+
 
 
 if __name__ == '__main__':

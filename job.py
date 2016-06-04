702af88724db51063c94f1e45ce4c69cdb04971a
Kevin LaFlamme <laflamme.kevin@gmail.com>
2016-06-04 18:53:14 -0400

    k

--- job.py
+++ job.py
@@ -11,21 +11,29 @@ logger = logging.getLogger('minder.job')
 
 
 def schedule_the_job(item):
-    logger.info('Aquiring lock before scheduling')
-    with db.lock('the_job_lock'):
-        scheduled = scheduler.enqueue_in(timedelta(seconds=Config.REMINDER_DELAY),
-                                         the_job,
-                                         item)
-        set_item('the_job', scheduled.id)
-    logger.info('Released lock after scheduling')
+    try:
+        logger.info('Aquiring lock before scheduling')
+        with db.lock('the_job_lock'):
+            scheduled = scheduler.enqueue_in(timedelta(seconds=Config.REMINDER_DELAY),
+                                             the_job,
+                                             item)
+            set_item('the_job', scheduled.id)
+        logger.info('Released lock after scheduling')
+    except:
+        logger.exception('Exception occurred')
+        raise
 
 
 def the_job(item):
-    logger.info('Running job')
-    send_sms(Config.USER_PHONE_NUMBER, 'Did you remember to turn off the {}'.format(item))
-    logger.info('Sent sms')
-    schedule_the_job(item)
-    logger.info('Scheduled job to run again')
+    try:
+        logger.info('Running job')
+        send_sms(Config.USER_PHONE_NUMBER, 'Did you remember to turn off the {}'.format(item))
+        logger.info('Sent sms')
+        schedule_the_job(item)
+        logger.info('Scheduled job to run again')
+    except:
+        logger.exception('Exception occurred')
+        raise
 
 
 def cancel_the_job():

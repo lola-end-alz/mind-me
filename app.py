9ce269bd9cd7cc70a07d9f85256d80c0a643a7cb
ankitdesai <ankitdesai88@gmail.com>
2016-06-04 12:26:43 -0400

    adding default echo response

--- app.py
+++ app.py
@@ -4,6 +4,30 @@ from flask import Flask, jsonify, request
 app = Flask('mind-me', static_url_path='')
 
 
+DEFAULT_ECHO_RESPONSE = {
+    'version': '1.0',
+    'response': {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': 'output speech for alexa'
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'HelloMinder',
+            'content': 'minder is working?!'
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': 'reprompting the message?'
+            }
+        },
+        'shouldEndSession': False
+    },
+    'sessionAttributes': {'source': 'minder'}
+}
+
+
 @app.route('/')
 def index():
     return jsonify(status='ok', data='hello')
@@ -11,7 +35,7 @@ def index():
 
 @app.route('/', methods=['POST'])
 def minder():
-    return jsonify(data=request.data)
+    return jsonify(DEFAULT_ECHO_RESPONSE)
 
 
 if __name__ == '__main__':

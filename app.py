90bbffdc36bdfe85c85cdb5f5c3468b155df978c
Cleo Schneider <cleo.m.schneider@gmail.com>
2016-06-04 14:34:09 -0400

    added oauth for calendar

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import logging
 import json
+import flask
+import httplib2
 from flask import Flask, jsonify, request
 
 from config import Config
 from log import configure
+from apiclient import discovery
+from oauth2client import client
+
+
 
 configure(Config.ENV)
 app = Flask('minder')
@@ -44,11 +50,6 @@ def _get_echo_response(speech_output, card_output, reprompt_message):
 def index():
     return jsonify(status='ok', data='hello')
 
-@app.route('/oauth2_callback')
-def oauth_redirect():
-	content = response.content
-	import pdb; pdb.set_trace()
-	return jsonify(status='ok', response=response.content)
 
 @app.route('/', methods=['POST'])
 def minder():
@@ -78,16 +79,42 @@ def _parse_request(request):
 
     return _get_echo_response(speech_output, card_output, reprompt_message)
 
+
 @app.route('/oauth2_callback')
-def oauth_redirect():
-	content = response.content
-	import pdb; pdb.set_trace()
-	return jsonify(status='ok', response=response.content)
+def oauth2_callback():
+	flow = client.flow_from_clientsecrets(
+	  'client_secret.json',
+	  scope='https://www.googleapis.com/auth/calendar.readonly',
+	  redirect_uri=flask.url_for('oauth2_callback', _external=True))
+
+	if 'code' not in flask.request.args:
+		auth_uri = flow.step1_get_authorize_url()
+		return flask.redirect(auth_uri)
+	else:
+		auth_code = flask.request.args.get('code')
+		credentials = flow.step2_exchange(auth_code)
+		flask.session['credentials'] = credentials.to_json()
+		return flask.redirect(flask.url_for('calendar'))
+
 
 @app.route('/calendar')
-def authenticate():
-	from calendar.oauth2 import connect_calendar
-	connect_calendar()
+def calendar():
+
+	if 'credentials' not in flask.session:
+		return flask.redirect(flask.url_for('oauth2_callback'))
+	
+	credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+	
+	if credentials.access_token_expired:
+		return flask.redirect(flask.url_for('oauth2_callback'))
+  	else:
+  		http_auth = credentials.authorize(httplib2.Http())
+    	calendar_service = discovery.build('calendar', 'v3', http_auth)
+    	response = calendar_service.events().list(calendarId='primary').execute()
+    	
+    	return jsonify(status='ok', events=response.get('items'))
 
 if __name__ == '__main__':
-    app.run(port=Config.PORT, debug=True)
+	import uuid
+	app.secret_key = str(uuid.uuid4())
+	app.run(port=Config.PORT, debug=True)
